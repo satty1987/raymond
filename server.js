3e1b0dfc9d7fcfe94084c726eecadea3019d4f9e
Satnam Singh <satnam.singh156@t-mobile.com>
2020-07-06 19:00:39 +0530

    update

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+var router = express.Router();
+
+app.use(express.static(__dirname + '/dist/raymond-app'));
+app.get('*', function (req, res) {
+  res.sendfile('./dist/raymond-app/index.html'); });
+
+port = process.env.PORT || 8080;
+
+app.listen(port);
+console.log('todo list RESTful API server started on: ' + port);
